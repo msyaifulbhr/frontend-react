1aa4d7755d3bbf9a164e7e8a6c64d658d2db9913
msyaifulbhr <syaifulyy@gmail.com>
2024-06-23 16:09:56 +0700

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../views/home/index.jsx";
+import Register from "../views/auth/register.jsx";
+import Login from "../views/auth/login.jsx";
+import Dashboard from "../views/admin/dashboard/index.jsx";
+import UsersIndex from "../views/admin/users/index.jsx";
+import UsersCreate from "../views/admin/users/create.jsx";
+import UsersEdit from "../views/admin/users/edit.jsx";
+
+export default function AppRoutes() {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    return (
+        <Routes>
+            {/* route "/" */}
+            <Route path="/" element={<Home />} />
+
+            <Route path="/register" element={
+                isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Register />
+            } />
+
+            <Route path="/login" element={
+                isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Login />
+            } />
+
+            <Route path="/admin/dashboard" element={
+                    isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
+            } />
+
+            <Route path="/admin/users" element={
+                isAuthenticated ? <UsersIndex /> : <Navigate to="/login" replace />
+            } />
+
+            <Route path="/admin/users/create" element={
+                isAuthenticated ? <UsersCreate /> : <Navigate to="/login" replace />
+            } />
+
+            <Route path="/admin/users/edit/:id" element={
+                isAuthenticated ? <UsersEdit /> : <Navigate to="/login" replace />
+            } />
+        </Routes>
+    );
+}
+
+
+
